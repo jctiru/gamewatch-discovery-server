9a9db653f5603c30e51cce7105e7df76503b1a5c
jctiru <jonathanchristiantiru@gmail.com>
2020-06-22 20:33:02 +0800

    Initial commit

---
+++ gamewatch-discovery-server/src/main/java/com/jctiru/gamewatch/api/gamewatchdiscoveryserver/GamewatchDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.jctiru.gamewatch.api.gamewatchdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamewatchDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamewatchDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ gamewatch-discovery-server/src/test/java/com/jctiru/gamewatch/api/gamewatchdiscoveryserver/GamewatchDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jctiru.gamewatch.api.gamewatchdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamewatchDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
